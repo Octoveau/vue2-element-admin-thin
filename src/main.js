b1d724e4f0e8ff199ce0ebc3566cee158acc8a87
looper <2390903545@qq.com>
2022-12-30 18:19:48 +0800

    添加cicd

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import './css/index.less'
 import './assets/icons'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-// 引入rem的适配文件，以1920作为参照，1rem就是80px
 import './flexible'
 
 Vue.config.productionTip = false
