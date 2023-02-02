0401f9bc6fd9ee57c0af79292ccc1b08d3593d42
cicada_fu <28926044+zero-fsc@users.noreply.github.com>
2023-02-02 23:45:24 +0800

    chore: change .cz-config.js

--- .cz-config.js
+++ .cz-config.js
@@ -5,11 +5,10 @@ module.exports = {
     { value: 'fix', name: 'fix[修复]:    修复一个Bug' },
     { value: 'docs', name: 'docs[文档]:    变更的只有文档' },
     { value: 'refactor', name: 'refactor[重构]:    代码重构，注意和特性、修复区分开' },
-    { value: 'test', name: 'test[测试]:    添加一个测试' },
     { value: 'chore', name: 'chore[工具]:    开发工具变动(构建、脚手架工具等)' },
     { value: 'revert', name: 'revert[回滚]:    代码回退' },
   ],
-  scopes: [{ name: 'leetcode' }, { name: 'javascript' }, { name: 'typescript' }, { name: 'Vue' }, { name: 'node' }],
+  scopes: [{ name: 'javascript' }, { name: 'typescript' }, { name: 'Vue' }, { name: 'node' }],
   // override the messages, defaults are as follows
   messages: {
     type: '选择一种你的提交类型:',
@@ -24,6 +23,5 @@ module.exports = {
   },
   allowCustomScopes: true,
   allowBreakingChanges: ['特性', '修复'],
-  // limit subject length
   subjectLimit: 100,
 };
